8ceb214a4ede3b84c85a66a10f7f2ea6263fc162
dellarosamarco <dellarosamarco00@gmail.com>
2022-01-28 20:49:25 +0100

    Added setup file

--- blockchain explorer/__init__.py
+++ blockchain explorer/__init__.py
@@ -1,26 +1,2 @@
-from setuptools import setup, find_packages
 from bitcoin import *
 from wallet import *
-
-setup(name='blockchain explorer',
-    version='1',
-    description='Python bitcoin blockchain tools',
-    long_description=open('README.md').read(),
-    author='Marco Della Rosa',
-    licene='MIT',
-    #author_email='',
-    url='https://github.com/dellarosamarco/Blockchain-Explorer',
-    install_requires=["hashlib","base58","codecs","ecdsa","binascii","mnemonic","bip32utils","requests","json","random"],
-    keywords=["python", "bitcoin", "blockchain", "crypto"],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Security :: Cryptography',
-    ],
-)
